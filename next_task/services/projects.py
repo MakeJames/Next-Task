669a1730df49b8d9cd1a042ea967028644ae6683
J-McB <jmcbean3@gmail.com>
2022-07-10 07:38:08 +0100

    FUNC: Find existing projects

--- next_task/services/projects.py
+++ next_task/services/projects.py
@@ -1,23 +1,32 @@
 """Module containing methods and classes for parsing projects."""
 
+import sys
+import re
+
 from datetime import datetime as dt
 
 from next_task.interface import console_output
 from next_task.services.models import Project
 from next_task.services.store import GetTasks, WriteTask
-from next_task.services.tasks import (CreateTask, GetNextTask, MarkAsClosed,
+from next_task.services.tasks import (GetNextTask, MarkAsClosed,
                                       SkipTask)
 
 
 class KeyGenerator:
     """Generate a project stub."""
 
-    def __init__(self, string):
+    def __init__(self, summary, file_data):
         """Instansiate the Key Generator Class."""
+        self.generate_key(summary)
+        if FindProjectId(id=self.id, data=file_data).found:
+            print(f"{self.id}: in project list.")
+
+    def generate_key(self, string):
+        """Generate the project key."""
         words = str(string).split()[:3]
         self.id = ""
         for word in words:
-            self.id += word[0]
+            self.id += word[0].upper()
 
 
 class CreateProject:
@@ -26,12 +35,43 @@ class CreateProject:
     def __init__(self, summary):
         """Instanisate the Create project class."""
         self.file_data = GetTasks().file_data
-        self.project_formatter(summary)
+        if FindProjectName(summary, data=self.file_data).found:
+            print("Project by that name exists.")
+            sys.exit([0])
+        self.project_formatter(summary, self.file_data)
         WriteTask(self.file_data)
         # console_output.Format(self.project).create_project()
 
-    def project_formatter(self, summary):
+    def project_formatter(self, summary, file_data):
         """Build the project dictionary."""
-        self.project = Project(KeyGenerator(summary).id,
+        self.project = Project(KeyGenerator(summary, file_data).id,
                                summary, dt.now()).__dict__
         self.file_data["projects"].append(self.project)
+
+
+class FindProjectId:
+    """Search for a project by id in a given list of projects."""
+
+    def __init__(self, id, data):
+        """Instansiate the class."""
+        for item in data["projects"]:
+            if item["id"] == str(id).upper():
+                self.data = item
+                self.found = True
+                return
+        self.found = False
+
+
+class FindProjectName:
+    """Search for a project by summary in a given list of projects."""
+
+    def __init__(self, summary, data):
+        """Instansiate the class."""
+        summary = re.sub(r'[^a-zA-Z0-9]', '', str(summary))
+        for item in data["projects"]:
+            item_summary = re.sub(r'[^a-zA-Z0-9]', '', item["summary"])
+            if item_summary.lower() == str(summary).lower():
+                self.data = item
+                self.found = True
+                return
+        self.found = False
--- tests/services/test_projects.py
+++ tests/services/test_projects.py
@@ -3,6 +3,7 @@
 import pytest
 import json
 from pytest_mock import mocker
+from pathlib import Path
 
 from next_task.services import projects
 
@@ -12,15 +13,28 @@ class TestKeyGenerator:
 
     def test_when_provided_a_string_a_key_is_returned(self):
         """R-BICEP: Right."""
-        assert projects.KeyGenerator("A Test Key").id == "ATK"
+        file_data = projects.GetTasks().file_data
+        assert projects.KeyGenerator("A Test Key", file_data).id == "ATK"
 
     def test_when_provided_an_integer_a_key_is_returned(self):
         """R-BICEP: Boundary."""
-        assert projects.KeyGenerator(500).id == "5"
+        file_data = projects.GetTasks().file_data
+        assert projects.KeyGenerator(500, file_data).id == "5"
 
 
 class TestCreateProject:
     """Test the method of the Project Creation class."""
+
+    @pytest.fixture
+    def mock_task_file(self, mocker) -> None:
+        """Return a mock task file."""
+        def mock_file_path():
+            return "tests/data_mocks/task_file"
+        mocker.patch.object(
+            Path,
+            "home",
+            return_value=mock_file_path()
+        )
 
     def test_that_project_is_created(self):
         """R-BICEP: Right."""
@@ -32,3 +46,102 @@ class TestCreateProject:
         test_call = projects.CreateProject(500).file_data["projects"][0]
         assert test_call["id"] == "5"
         assert test_call["summary"] == "500"
+
+    def test_when_project_summary_exists_in_file_then_sys_exit(
+        self,
+        capsys,
+        mock_task_file
+    ):
+        """R-BICEP: Right."""
+        with pytest.raises(SystemExit):
+            projects.CreateProject("A Test Project")
+        captured = capsys.readouterr()
+        assert "Project by that name exists" in captured.out
+
+
+class TestFindProjectId:
+    """Test the method of the Find Project class."""
+
+    @pytest.fixture
+    def mock_task_file(self, mocker) -> None:
+        """Return a mock task file."""
+        def mock_file_path():
+            return "tests/data_mocks/task_file"
+        mocker.patch.object(
+            Path,
+            "home",
+            return_value=mock_file_path()
+        )
+
+    def test_when_project_id_exists_then_found_is_true(self, mock_task_file):
+        """R-BICEP: Right."""
+        with open("tests/data_mocks/task_file/.tasks.json", "r") as file:
+            data = json.load(file)
+        assert projects.FindProjectId("ATP", data).found is True
+
+    def test_when_project_id_case_is_wrong_then_found_is_true(
+        self,
+        mock_task_file
+    ):
+        """R-BICEP: Right."""
+        with open("tests/data_mocks/task_file/.tasks.json", "r") as file:
+            data = json.load(file)
+        assert projects.FindProjectId("atp", data).found is True
+
+    def test_when_provided_id_is_int_then_search_executes_and_found_is_false(
+        self,
+        mock_task_file
+    ):
+        """R-BICEP: Boundary."""
+        with open("tests/data_mocks/task_file/.tasks.json", "r") as file:
+            data = json.load(file)
+        assert projects.FindProjectId(5, data).found is True
+
+
+class TestFindProjectName:
+    """Test the method of the Find Project class."""
+
+    @pytest.fixture
+    def mock_task_file(self, mocker) -> None:
+        """Return a mock task file."""
+        def mock_file_path():
+            return "tests/data_mocks/task_file"
+        mocker.patch.object(
+            Path,
+            "home",
+            return_value=mock_file_path()
+        )
+
+    def test_when_project_id_exists_then_found_is_true(self, mock_task_file):
+        """R-BICEP: Right."""
+        with open("tests/data_mocks/task_file/.tasks.json", "r") as file:
+            data = json.load(file)
+        assert projects.FindProjectName("A Test Project.", data).found is True
+
+    def test_when_project_id_case_is_wrong_then_found_is_true(
+        self,
+        mock_task_file
+    ):
+        """R-BICEP: Right."""
+        with open("tests/data_mocks/task_file/.tasks.json", "r") as file:
+            data = json.load(file)
+        assert projects.FindProjectName("a test project.", data).found is True
+
+    def test_when_provided_id_is_int_then_search_executes_and_found_is_false(
+        self,
+        mock_task_file
+    ):
+        """R-BICEP: Boundary."""
+        with open("tests/data_mocks/task_file/.tasks.json", "r") as file:
+            data = json.load(file)
+        assert projects.FindProjectName(500, data).found is True
+
+    def test_when_additional_non_alphnum_characters_then_found_is_true(
+        self,
+        mock_task_file
+    ):
+        """R-BICEP: Boundary."""
+        with open("tests/data_mocks/task_file/.tasks.json", "r") as file:
+            data = json.load(file)
+        test_name = "_a* Test ?project&^."
+        assert projects.FindProjectName(test_name, data).found is True
