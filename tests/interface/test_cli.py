75cbd0e44573b2a14778e90370051bb9d0cfb392
MakeJames <james@makejames.com>
2022-09-25 23:10:26 +0100

    REFACTOR: remove redundant fixture

--- tests/interface/test_cli.py
+++ tests/interface/test_cli.py
@@ -5,24 +5,11 @@ import pytest
 
 from pytest_mock import mocker
 
-from next_task.interface import cli
-from next_task.services import store
 from next_task import __version__
 
 
 class TestCliMainMethod:
     """Test the main method of the cli module."""
-
-    @pytest.fixture(autouse=True)
-    def mock_write(self, mocker):
-        """Mock the write aspect of the writer function."""
-        def mock_function():
-            return None
-        mocker.patch.object(
-            store.WriteTask,
-            "__init__",
-            return_value=mock_function()
-        )
 
     @pytest.fixture
     def mock_get_next_task(self, mocker):
