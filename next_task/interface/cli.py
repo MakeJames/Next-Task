e7dd33841305cd9b0d85387455625958963002d8
MakeJames <james@makejames.com>
2022-10-04 18:55:12 +0100

    FUNC: update CLI targets and help text

--- next_task/interface/cli.py
+++ next_task/interface/cli.py
@@ -1,14 +1,34 @@
 """Command line script."""
 
 import argparse
-import sys
 
 import next_task
 
 
-def main(argv=None):
+def parse_args(argv=None):
     """Provide options for the package."""
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="""Next Task is a task management solution for those that
+want to avoid the faff.
+
+Task mnanagement is a mundane feature of life with paper form task
+lists being superior to must task management solutions for simplicity.
+Many graphically focused task management solutions focus on the design
+of the task or custom workflow rather than focusing on getting the task
+done.
+
+This application focuses on serving a user with a task to either progress
+or skip.
+
+Tasks are a discrete unit of work that can be actioned in isolation.
+They should be identifable and specific so that you can
+Some tasks might read as
+\t- "email Frank the notes from yesterday's meeting"
+\t- "Review the latest quarterly notes"
+        """,
+        usage='use "%(prog)s --help" for more information',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
     group = parser.add_mutually_exclusive_group()
     parser.add_argument(
         "-v",
@@ -17,67 +37,59 @@ def main(argv=None):
         action="version",
         version=f"Next: {next_task.__version__}"
     )
-    parser.add_argument(
+    group.add_argument(
         "-p",
         "--project",
-        help="""
-            Targest a given project
+        help="""Set a project as active as [Next -p 'Project Name']. Projects
+remain active until cleared (with -c), or a new project is selected.
+To view the current active project use [Next -p].
+
+If the project doesn't exist, or the user enters the wrong name,
+the user will be prompted to create the project or select from a
+currently existing project.
+
+All task related actions will be performed within the project
+eg:
+\t Next -a "A new task" creates a new task in the project.
         """,
         nargs="?",
         const=True
     )
     group.add_argument(
+        "-ap",
+        "--add_project",
+        help="Create a project [Next --add_project 'Project Name'",
+        type=str
+    )
+    group.add_argument(
         "-a",
         "--add",
-        help="""
-            Create a task in the format --add 'Your Task name'.
-            Adds one task at a time.
-        """,
+        help="Create a task [Next --add 'Your Task name']",
         type=str
     )
     group.add_argument(
         "-t",
         "--task",
-        help="""
-            Returns the next task to be done
-        """,
+        help="Returns the next task to be done",
         action="store_true"
     )
     group.add_argument(
         "-d",
         "--done",
-        help="""
-            Mark the current task as done
-        """,
+        help="Mark the current task as done",
         action="store_true"
     )
     group.add_argument(
         "-s",
         "--skip",
-        help="""
-            Skips the current top task.
-        """,
+        help="Skips the current top task.",
         action="store_true"
     )
     group.add_argument(
         "-c",
         "--clear",
-        help="""
-            Clear the current project and return
-            to the normal task list
-        """,
+        help="Clear the current project to return to the global task list",
         action="store_true"
     )
 
-    args = parser.parse_args(argv)
-
-    # 1. set project / return current project
-    # one of:
-    # add -- just add task
-    # task
-    # done
-    # skip
-    # clear
-    print(vars(args))
-
-    sys.exit()
+    return parser.parse_args(argv)
