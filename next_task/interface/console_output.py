a3a152f9091e978505464028d5d9b92d5aedac9f
MakeJames <james@makejames.com>
2022-10-08 14:14:31 +0100

    REFACTOR: Console output methods

--- next_task/interface/console_output.py
+++ next_task/interface/console_output.py
@@ -9,148 +9,36 @@ from rich.theme import Theme
 class Style:
     """Hold the Theme definitions for the package."""
 
-    def __init__(self):
-        """Instansiate the class."""
-        task_theme = Theme({
-                "default": "default",
-                "info": "#C69400",
-                "warning": "#C92727",
-                "highlight": "#ADD9F4",
-                "number": "#ADD9F4",
-                "pass": "#4A70C2",
-                "green": "#09814A"
-            }, inherit=False)
-        self.console = Console(theme=task_theme)
+    task_theme = Theme(
+        {
+            "default": "default",
+            "info": "#C69400",
+            "warning": "#C92727",
+            "highlight": "#ADD9F4",
+            "green": "#09814A"
+        },
+        inherit=True
+    )
+
+    def console(self):
+        """Instnasiate the Console Class."""
+        return Console(theme=self.task_theme)
 
 
-class ListTasks:
+class TaskTable:
     """Returns a formatted table of tasks to the command line."""
 
     def __init__(self, data: list, title="Open Tasks"):
         """Insttansiate the class."""
-        if data is []:
+        if data is None:
             return
-
         table = Table(title=title)
-
         table.box = box.SIMPLE_HEAD
-
-        table.add_column("id", style="#09814A", no_wrap=True)
-        table.add_column("summary", style="#4A70C2", no_wrap=True)
-        if title == "Open Tasks":
-            table.add_column("due", justify="right", style="#4A70C2")
-            table.add_column("skip_count", justify="right", style="#4A70C2")
-            for row in data:
-                table.add_row(
-                    f"{row['id']}",
-                    f"{row['summary']}",
-                    # f"{TimeStamp().short(row['due'])}",
-                    f"{row['skip_count']}",
-                )
-        else:
-            table.add_column("created", justify="right", style="#4A70C2")
-            for row in data:
-                table.add_row(
-                    f"{row['id']}",
-                    f"{row['summary']}",
-                    # f"{TimeStamp().short(row['created'])}",
-                )
-
+        table.add_column("Id", style="#C69400", no_wrap=True)
+        table.add_column("Summary", style="#ADD9F4", no_wrap=True)
+        for row in data:
+            table.add_row(
+                f"{row[0]}",
+                f"{row[1]}",
+            )
         Console().print(table, justify="left")
-
-
-class Format:
-    """Pre-formatted rich styled output."""
-
-    def __init__(self, data):
-        """Instansiate the class."""
-        self.data = data
-        self.id = data["id"]
-        self.summary = data["summary"]
-        self.due = data["due"]
-
-    def create_task(self):
-        """Format text output for create task output."""
-        Style().console.print(
-            f"[b]Created task {self.id}: [/b]"
-            f"[highlight]{self.summary}[/highlight]\n"
-            f"Due: {self.due}",
-            style="info"
-        )
-
-    def next_task(self):
-        """Format console output for returning the next task."""
-        Style().console.print(
-            f"[b][#5CE521]{self.id}:[/#5CE521] {self.summary}[/b]\n"
-            f"[warning]Due:[/warning] [default]{self.due}[/default]",
-            style="pass"
-        )
-
-    def skip_task(self):
-        """Format console output when skiping the next task."""
-        Style().console.print(
-            f"[warning]updated {self.id}:[/warning] {self.summary}\n"
-            f"[info]now skiped: {self.data['skip_count']}[/info]",
-            style="pass"
-        )
-
-    def mark_closed(self):
-        """Format console output for closing a task."""
-        Style().console.print(
-            f"[b]Updated {self.id},[/b] "
-            f"Completed: [default]{self.data['completed']}[/default]",
-            style="green"
-        )
-
-
-class Congratulations:
-    """Format console output when there are no more tasks to do."""
-
-    def __init__(self):
-        """Instansiate the class."""
-        Style().console.print(
-            "[b]Congratulations![/b]\n"
-            "There are no tasks on your to do list\n"
-            "Take a break and have a cup of tea.",
-            style="green"
-        )
-
-
-class FormatProject:
-    """Format the output for projects."""
-
-    def __init__(self, data):
-        """Instansiate the class."""
-        self.data = data
-        self.id = data["id"]
-        self.summary = data["summary"]
-
-    def create(self):
-        """Format text output for create task output."""
-        Style().console.print(
-            f"[b]Created project {self.id}: [/b]"
-            f"[highlight]{self.summary}[/highlight]",
-            style="info"
-        )
-
-
-class RaiseErrorMessage:
-    """Handle error scenarios."""
-
-    def __init__(self, free_text=None):
-        """Instansiate the class."""
-        if free_text:
-            Style().console.print(f"[warning]{free_text}[/warning]")
-
-    def input_error(self):
-        """Catching edge cases or undesired behaviour."""
-        Style().console.print(
-            "[warning]Invalid argument combination[/warning]"
-        )
-
-    def project_exists(self, project):
-        """Pre-formatted error messaging for existing project."""
-        Style().console.print(
-            f"[warning]A project '{project.summary}' already "
-            f"exists with project key: {project.id}"
-        )
